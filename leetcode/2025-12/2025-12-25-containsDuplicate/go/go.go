092e6ec65fbb70625051e4e26215bac6b825eeb8
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-12-25 18:19:32 +0330

    leetcode solved with python and golang, TODO: solve with javascript

---
+++ leetcode/2025-12/2025-12-25-containsDuplicate/go/go.go
@@ -0,0 +1,14 @@
+package main
+
+func containsDuplicate(nums []int) bool {
+	var seen map[int]bool
+	seen = make(map[int]bool)
+
+	for i := 0; i < len(nums); i++ {
+		if _, exist := seen[nums[i]]; exist {
+			return true
+		}
+		seen[nums[i]] = true
+	}
+	return false
+}
---
+++ leetcode/2025-12/2025-12-25-containsDuplicate/py/python.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set_of_nums = set(nums)
+        if len(nums) == len(set_of_nums):
+            return False
+        return True
